8585276c7bfea3e129856c5c885d7eacd9089175
Matt <matt.nelsenn@gmail.com>
2019-11-05 21:36:27 -0600

    final commit on 11-5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def all_products_method
-    @products = Product.new
+    @products = Product.all
     render "products.json.jb"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
-Product.new(name: "nike", price: 100, image_url: "nike.com", description: "super cool shoe")
-Product.new(name: "adidas", price: 75, image_url: "adidas.com", description: "a pretty decent shoe")
-Product.new(name: "reebok", price: 45, image_url: "reebok.com", description: "the best shoe")
+shoe = Product.new(name: "nike", price: 100, image_url: "nike.com", description: "super cool shoe")
+shoe.save
+shoe = Product.new(name: "adidas", price: 75, image_url: "adidas.com", description: "a pretty decent shoe")
+shoe.save
+shoe = Product.new(name: "reebok", price: 45, image_url: "reebok.com", description: "the best shoe")
+shoe.save

2baf7a26718de052e24539c1625afe670f306620
Matt <matt.nelsenn@gmail.com>
2019-11-06 21:27:49 -0600

    first product method

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,11 @@
 class Api::ProductsController < ApplicationController
   def all_products_method
     @products = Product.all
-    render "all_products.json.jb"
+    render "products.json.jb"
+  end
+
+  def first_product_method
+    @product = Product.first
+    render "first_product.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "all_products_url" => "products#all_products_method"
+    get "first_product_url" => "products#first_product_method"
   end
 end

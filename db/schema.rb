a243db0a5f022e48932eb66fa7a42a55b027d7eb
Matt <matt.nelsenn@gmail.com>
2019-11-12 20:06:29 -0600

    Changed price to decimal, changed description to text, added In stock or out of stock

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_020511) do
+ActiveRecord::Schema.define(version: 2019_11_13_020322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 5, scale: 2
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "InStockorOutofStock"
   end
 
 end

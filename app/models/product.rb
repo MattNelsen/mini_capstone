fbd4e06ad32b6343adc6a7996ecc10682c844ef3
Matt <matt.nelsenn@gmail.com>
2019-11-18 19:31:22 -0600

    sdded has_many, belongs_to

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,13 +6,15 @@ class Product < ApplicationRecord
   validates :description, length: { in: 3..75 }
   validates :image_url, presence: true
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
+  belongs_to :supplier
 
-  def images
-    Image.where(product_id: id)
-  end
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end
 
   def is_discounted?
     if price < 10

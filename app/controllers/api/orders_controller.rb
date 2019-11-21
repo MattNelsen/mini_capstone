596edffce05815d77f58d865e5d21be1ac06ae88
Matt <matt.nelsenn@gmail.com>
2019-11-20 18:18:30 -0600

    fixed orders controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,13 +1,14 @@
 class Api::OrdersController < ApplicationController
   def create
     @order = Order.new(
-      user_id: [:user_id],
+      user_id: [:current_user_id],
       product_id: params[:product_id],
       quantity: params[:quantity],
       subtotal: [:subtotal],
       tax: [:tax],
       total: [:total],
     )
+    @order.save
     render "orders.json.jb"
   end
 end

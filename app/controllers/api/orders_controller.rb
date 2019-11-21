fbc570d4e59750ccfcca9fa92b837d3eec75dce3
Matt <matt.nelsenn@gmail.com>
2019-11-20 18:50:30 -0600

    added calculations

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,14 +1,20 @@
 class Api::OrdersController < ApplicationController
   def create
+    product = Product.find_by(id: params[:product_id])
+    price = product.price
+    calculated_subtotal = params[:quantity].to_i * price
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     @order = Order.new(
       user_id: [:current_user_id],
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: [:subtotal],
-      tax: [:tax],
-      total: [:total],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
     @order.save
-    render "orders.json.jb"
+    render "show.json.jb"
   end
 end

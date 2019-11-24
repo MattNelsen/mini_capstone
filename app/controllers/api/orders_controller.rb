ddea870f41c1fae5527b66b3d901d3d28b1da6df
Matt <matt.nelsenn@gmail.com>
2019-11-24 13:25:02 -0600

    created carted_products controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -12,20 +12,23 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
-    price = product.price
-    calculated_subtotal = params[:quantity].to_i * price
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
+    # product = Product.find_by(id: params[:product_id])
+    # price = product.price
+    # calculated_subtotal = params[:quantity].to_i * price
+    # calculated_tax = calculated_subtotal * 0.09
+    # calculated_total = calculated_subtotal + calculated_tax
+    calculated_subtotal = 0
+    calculated_tax = 0
+    calculated_total = 0
+    @carted_products = CartedProduct.where(status: "carted")
 
     @order = Order.new(
-      user_id: [:current_user_id],
-      product_id: params[:product_id],
-      quantity: params[:quantity],
+      user_id: current_user.id,
       subtotal: calculated_subtotal,
       tax: calculated_tax,
       total: calculated_total,
     )
+
     @order.save
     render "show.json.jb"
   end

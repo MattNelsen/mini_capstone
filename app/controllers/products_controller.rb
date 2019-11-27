b61ad5857ede259170cc2e6e74a82edcbe92ce04
Matt <matt.nelsenn@gmail.com>
2019-11-26 21:24:56 -0600

    made product create with html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,17 @@ class ProductsController < ApplicationController
     render "new.html.erb"
   end
 
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      supplier_id: params[:supplier_id],
+    )
+    @product.save
+    redirect_to "/products"
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
     render "show.html.erb"
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   get "/products" => "products#index"
   get "/products/new" => "products#new"
+  post "/products" => "products#create"
   get "/products/:id" => "products#show"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
